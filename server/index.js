449dbdf6204422f5959e0ba6aa153d2cd9acb328
Vaishnavi Mokadam <vaishnavimokadam@gmail.com>
2023-06-13 00:47:25 +0530

    add

--- server/index.js
+++ server/index.js
@@ -11,3 +11,6 @@ app.use(express.json({ liming: "50mb" }));
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Hello from DALL.E" });
 });
+
+app.listen(8080, () => console.log("Server has started on port 8080"));
+ 
